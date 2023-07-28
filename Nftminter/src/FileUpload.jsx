cdac247fa524861cdd0ab16a3f523ccdc7eefe0b
shlok <shlok@pop-os.localdomain>
2023-07-28 23:29:08 +0530

    commit

---
+++ Nftminter/src/FileUpload.jsx
@@ -0,0 +1,10 @@
+const FileUpload = () => {
+    return <>
+        <div className="form">
+            <form action="">
+                <input type="text" className="choose" />
+                <button className="btn">Nft minter</button>
+            </form>
+        </div>
+    </>
+}
